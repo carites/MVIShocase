9701e9967318b635a89c1b5b2382991b082f1c9c
Riccardo Testa <riccardo.testa.rt@gmail.com>
2021-02-02 17:46:57 +0100

    Fix naming

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,7 +8,7 @@ func greet() -> String {
 struct ContentView: View {
     var state: AppState
     var sharedModel: KMPViewModel
-    @State private var username: String = ""
+    @State private var name: String = ""
     var body: some View {
         NavigationView {
             VStack {
@@ -18,14 +18,14 @@ struct ContentView: View {
                 
                 Text(state.personNameLabelText)
                 TextField(
-                        "User name (email address)",
-                         text: $username
-                ).onChange(of: username, perform: { value in
+                        "New Person name",
+                         text: $name
+                ).onChange(of: name, perform: { value in
                     sharedModel.personNameChanged(name: value)
                 }).padding(EdgeInsets(top: 8, leading: 32, bottom: 8, trailing: 32))
                 Button("Create Name", action: {
-                    sharedModel.createButtonPressed(name: username)
-                    self.username = ""
+                    sharedModel.createButtonPressed(name: name)
+                    self.name = ""
                 })
             }.textFieldStyle(RoundedBorderTextFieldStyle())
             
